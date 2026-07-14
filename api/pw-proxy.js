edac763626aeb67f7d45153a1c960e1a15d0ceac
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-07-14 08:22:51 +0530

    Update pw-proxy.js

--- api/pw-proxy.js
+++ api/pw-proxy.js
@@ -3,18 +3,12 @@ const axios = require('axios');
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     
-    // req.url mein /api/pw-proxy/aHR0cHM.../master.mpd?Signature=... milega
-    // Hume ise rangexcoder-backend se jodhna hai
     const targetBase = "https://rangexcoder-backend.onrender.com";
     const targetUrl = targetBase + req.url;
 
     try {
         const response = await axios.get(targetUrl, {
-            responseType: 'arraybuffer',
-            headers: {
-                'Referer': 'https://brainboxinstitute.in/',
-                'User-Agent': 'Mozilla/5.0'
-            }
+            responseType: 'arraybuffer'
         });
 
         res.setHeader('Content-Type', response.headers['content-type']);
