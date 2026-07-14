e9983cca68b248ef3e743a766cf93cfa70258a2e
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-07-14 12:12:03 +0530

    Update allbatches.js

--- allbatches.js
+++ allbatches.js
@@ -5,9 +5,15 @@ export default async function handler(req, res) {
     const SECRET_KEY = "pwzone_super_secret_key_2026";
     try {
         const response = await axios.get('https://rangexcoder-backend.onrender.com/api/AllBatches?page=2');
+        
+        console.log("Status:", response.status);
+        console.log("Data:", response.data);
+
         const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(response.data), SECRET_KEY).toString();
+        
         res.status(200).json({ encryptedData });
     } catch (error) {
+        console.error("Error:", error.message);
         res.status(500).json({ error: "Failed to fetch or encrypt data" });
     }
 }
