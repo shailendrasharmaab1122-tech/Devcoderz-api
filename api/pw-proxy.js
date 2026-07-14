8364cab551c31cd9822da69cd772ad243446396d
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-07-14 08:26:05 +0530

    Update pw-proxy.js

--- api/pw-proxy.js
+++ api/pw-proxy.js
@@ -2,18 +2,21 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
     const targetBase = "https://rangexcoder-backend.onrender.com";
-    const targetUrl = targetBase + req.url;
+    const fullTargetUrl = targetBase + req.url;
 
     try {
-        const response = await axios.get(targetUrl, {
+        const response = await axios.get(fullTargetUrl, {
             responseType: 'arraybuffer'
         });
 
         res.setHeader('Content-Type', response.headers['content-type']);
         res.status(200).send(Buffer.from(response.data));
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(error.response ? error.response.status : 500).json({ 
+            error: error.message 
+        });
     }
 };
